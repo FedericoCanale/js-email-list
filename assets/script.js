f2d459697175088ab95b0ce29fd8045a1b46f702
FedericoCanale <icocanale@gmail.com>
2025-10-23 15:31:51 +0200

    add to do list and tools for js

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+/* To do list
+ - Creare un array vuoto emails per salvare le email
+ - Creare un ciclo for che si ripete 10 volte
+ - Prendere le mail con axios
+ - Aggiungere ogni email all'array con push
+ - Controllare se l'array ha raggiunto 10 elementi 
+ - Creare un elemento <ul> con document.createElement()
+ - Usare forEach() per scorrere l'array delle email
+ - Per ogni email, creare un elemento <li>
+ - Inserire il testo dell'email dentro il <li>
+ - Aggiungere ogni <li> all'<ul>
+ - Aggiungere l'<ul> completo al body
+
+ Tools
+  - Axios
+  - Boolean API
+  - push 
+  - forEach
+  - document.createElement()
+  - element.textContent
+  - element.appendChild()
+  - axios.get()
+  - .then()
+  - .catch()
+  */
