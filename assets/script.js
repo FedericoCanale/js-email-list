18f77c9ab3e731a3895a3ddb043364dd22dd882a
FedericoCanale <icocanale@gmail.com>
2025-10-23 15:41:46 +0200

    add push email in the new array

--- assets/script.js
+++ assets/script.js
@@ -28,5 +28,8 @@
 const emails = [];
 // Loop per generare 10 elementi
 for (let i = 0; i < 10; i++) {
-
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => {
+      // Aggiungo l'email all'array
+      emails.push(response.data.response);})
 }
